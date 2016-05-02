7bc1045b91a163fc516ba8091d4c71309651fda1
BuringBright <lcg51271@gmail.com>
2016-05-02 21:57:42 +0800

    /** adjust function deploy, adjust icon */

--- javascripts/Bubbles.js
+++ javascripts/Bubbles.js
@@ -1,3 +1,16 @@
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
 /**
  * js describe the real world's object
  * vector object
